9feb91e90d7e815443e6d03f4857bfa121b1d822
Daniel Cepeda <dcep93@gmail.com>
2018-06-20 10:33:03 -0700

    update pull

--- app/etc/pull.sh
+++ app/etc/pull.sh
@@ -4,6 +4,8 @@ exec &> >(tee -a /var/log/socket_games.log)
 
 echo "$(date) $(pwd) $(whoami) pull"
 git pull
-code=$?
-echo "$(date) $code pulled"
+code1=$?
+git submodule update --init --recursive --remote
+code2=$?
+echo "$(date) $code1 $code2 pulled"
 echo
